76561a96bf672a8cedae9456f1bf57054e33f413
soso-light <80017031+soso-light@users.noreply.github.com>
2022-09-02 15:06:26 +0900

    implements interface to WordCRUD

--- src/main/java/com/practialproject/word/WordCRUD.java
+++ src/main/java/com/practialproject/word/WordCRUD.java
@@ -1,4 +1,24 @@
 package com.practialproject.word;
 
-public class WordCRUD {
+public class WordCRUD implements ICRUD {
+
+    @Override
+    public Object add() {
+        return null;
+    }
+
+    @Override
+    public int update(Object obj) {
+        return 0;
+    }
+
+    @Override
+    public int delete(Object obj) {
+        return 0;
+    }
+
+    @Override
+    public void selectOne(int id) {
+
+    }
 }
