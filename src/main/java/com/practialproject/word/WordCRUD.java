44c96851ce2abe3c3b66c89f35b132e79f6b7e7d
soso-light <80017031+soso-light@users.noreply.github.com>
2022-09-02 18:13:45 +0900

    start add

--- src/main/java/com/practialproject/word/WordCRUD.java
+++ src/main/java/com/practialproject/word/WordCRUD.java
@@ -1,9 +1,25 @@
 package com.practialproject.word;
 
 public class WordCRUD implements ICRUD {
+    ArrayList<List> list;
+    /*
+    >> 난이도(1,2,3) & 새 단어 입력 : 1 driveway
+     */
+
+    public WordCRUD(Scanner s) {
+        list = new ArrayList<>();
+        this.s = s;
+    }
 
     @Override
     public Object add() {
+        System.out.println(">>> 난이도(1,2,3) & 새 단어 입력 : ");
+        int level = s.nextInt();
+        String word = s.next();
+        System.out.println("\t뜻 입력 : ");
+        string meaning = s.nextLine();
+        System.out.println(">>> ");
+
         return null;
     }
 
