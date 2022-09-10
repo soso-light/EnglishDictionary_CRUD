2f327d92c2d1afe6ad3b7cca9dfbe6df30cbf03c
soso-light <80017031+soso-light@users.noreply.github.com>
2022-09-10 19:19:08 +0900

    give example on update / delete

--- src/com/practialproject/word/WordCRUD.java
+++ src/com/practialproject/word/WordCRUD.java
@@ -32,14 +32,42 @@ public class WordCRUD implements ICRUD {
         System.out.println("* 새 단어가 단어장에 추가되었습니다. *\n");
     }
 
+    /* update
+    >> 수정할 단어 검색 : pa
+    ----------------------------------
+    1 * parent 부모님
+    2 * parrot 앵무새
+    ----------------------------------
+    > 수정할 번호 선택 : 1
+    >> 뜻 입력 : 부모, 부모님
+    ** 단어가 수정되었습니다 **
+     */
     @Override
     public int update(Object obj) {
         return 0;
     }
 
+    public void updateItem(){
+
+    }
+
+    /* delete
+    >> 삭제할 단어 검색 : pa
+    ----------------------------------
+    1 * parent 부모, 부모님
+    2 * parrot 앵무새
+    ----------------------------------
+    > 삭제할 번호 선택 : 1
+    >> 정말로 삭제하시겠습니까(y/n)? 
+    ** 단어가 삭제되었습니다 **
+     */
     @Override
     public int delete(Object obj) {
         return 0;
+    }
+
+    public void deleteItem(){
+
     }
 
     @Override
